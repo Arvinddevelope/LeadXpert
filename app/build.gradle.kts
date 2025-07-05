e031bb0ee318f9e09b7637392786c503e2a10674
Arvind Kumar <arvindfinance625@gmail.com>
2025-07-05 10:32:58 +0530

    Lead Managment App

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,6 @@ android {
         applicationId = "com.arvind.leadxpert"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
         versionCode = 2
         versionName = "2.0"
 
